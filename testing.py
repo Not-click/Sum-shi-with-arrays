01937395a35d7c6a5098eb3a1c394973288a95e0
ClickGMD <lunaawasti03@gmail.com>
2026-03-31 17:58:45 +0545

    Rewrote the entire Slist class

--- testing.py
+++ testing.py
@@ -1,6 +1,7 @@
 from main import *
 
 myArray = Slist(0,1,2,3,4,5,6,7,8,9)
+mySecondArray = Slist(9,8,7,6,5,4,3,2,1,0,7,2,5)
 
-print(myArray + 1)
-print(1 + myArray)
+print(myArray + mySecondArray)
+print(myArray + 0.5)
